de895c035f967f278bfeeb51b7bcc4ccc46f3e3e
matnut2 <62598618+matnut2@users.noreply.github.com>
2021-11-09 15:19:54 +0100

    Update zeroOneLecteralConverter.py

--- code/zeroOneLecteralConverter.py
+++ code/zeroOneLecteralConverter.py
@@ -1,4 +1,4 @@
-# This program is used to convert ZERO and ONE to 0 and 1
+# This program is used to convert ZERO and ONE to 0 and 1. This version can also consider the space character.
 # After the elaboration, the output will be sent to 'binary.txt'
 
 f = open('zero_one', 'r')          # Insert here input file
@@ -8,7 +8,7 @@ f.close()
 i = 0
 
 
-with open('binary.txt', 'w') as o:
+with open('binary.txt', 'w') as o:      # Create a new file called 'binary.txt' to write the output
     while i < len(string):
         if string[i:i+4] == 'ZERO':
             o.write('0')
