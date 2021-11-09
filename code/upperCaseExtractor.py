77c742ad0c23963fbf0ebaea1fd32701f4e3a803
matnut2 <62598618+matnut2@users.noreply.github.com>
2021-11-09 15:18:59 +0100

    Update upperCaseExtractor.py

--- code/upperCaseExtractor.py
+++ code/upperCaseExtractor.py
@@ -1,11 +1,11 @@
 # This program is used to extract the uppercase characters from an input file
 # After the extraction, the result is placed in a file called 'uppercase.txt'
 
-f = open('challenge.txt', 'r')              # Insert here input file
+f = open('inputFile.txt', 'r')              # Insert here input file
 book = f.read()
 f.close()
 
-with open('uppercase.txt', 'w') as o:
+with open('uppercase.txt', 'w') as o:       # Create a new file named 'uppercase.txt' to place to write the output
     for i in book:
         if (i.isupper() == True):
             o.write(i)
