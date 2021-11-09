3e60dff303f01822c3569fe5160d885d60a6d171
matnut2 <62598618+matnut2@users.noreply.github.com>
2021-11-09 15:17:52 +0100

    Update caesar.py

--- code/caesar.py
+++ code/caesar.py
@@ -1,7 +1,7 @@
 # This program is used to shift the characters from an input to get the desired string
 
 string  = ''            # Insert here your String
-key     = 0            # Insert here your Shift Value
+key     = 0             # Insert here your Shift Value
 output  = ''            # DON'T TOUCH THIS
 
 for i in range(len(string)):
