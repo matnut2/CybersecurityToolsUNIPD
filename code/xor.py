f2342080aec39027341e2ad33799e1921cd7ae33
matnut2 <62598618+matnut2@users.noreply.github.com>
2021-11-10 14:51:26 +0100

    Update xor.py

--- code/xor.py
+++ code/xor.py
@@ -1,23 +1,25 @@
+# This program was created to solve a challenge where the known information were: original data, encrypted data and encrypted flag.
+# If your case doesn't include a Base64 encoding, you can just sign row 16 and 17 as comment (# in python for single line comment)
+
 import base64
 
-original_data = "El Psy Congroo"
-encrypted_data = "IFhiPhZNYi0KWiUcCls="
-encrypted_flag = "I3gDKVh1Lh4EVyMDBFo="
+# Your Input Here
+original_data  = ""         
+encrypted_data = ""
+encrypted_flag = ""
 
 def base64tostring(text):
     return base64.b64decode(text).decode('utf-8', errors="ignore")
 
 
 #decode the encryption from base64
-enc_data= base64tostring(encrypted_data)
-enc_flag= base64tostring(encrypted_flag)
+enc_data = base64tostring(encrypted_data)
+enc_flag = base64tostring(encrypted_flag)
 
 #we know apply the xor to obtain the key
 key = ''.join([chr(ord(x) ^ ord(y))for x, y in zip(original_data, enc_data)])
 
 print('key:\t',key)
-#this seems a reasonalble key
 
 flag = ''.join([chr(ord(x) ^ ord(y))for x, y in zip(enc_flag, key)])
 print("Flag:\t", flag)
-#flag: FLAG=Alpacaman
